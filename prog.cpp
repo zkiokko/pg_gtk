1985793cf1977d6fb574e04e9bb638bc968e92d1
zkiokko <schiocco56@gmail.com>
2022-11-17 22:34:03 +0100

    0.0.2  button and label

--- prog.cpp
+++ prog.cpp
@@ -1,11 +1,21 @@
 #include "gtk/gtk.h"
 
+static void onclick1(GtkButton *btn, gpointer data){
+    GtkWidget *lib;
+    GtkWindow *win =GTK_WINDOW(data);
+    gtk_window_destroy(win);
+    /*lib =gtk_label_new("ciao");
+    gtk_window_set_child(GTK_WINDOW(win), lib);*/
+}
 static void activate(GtkApplication *app, gpointer user_data){
     g_print("hih");
-    GtkWidget *win;
+    GtkWidget *win, *btn;
     win = gtk_application_window_new(GTK_APPLICATION(app));
     gtk_window_set_title(GTK_WINDOW(win),"app(alpha) 0.0.1");
     gtk_window_set_default_size(GTK_WINDOW(win),400,300);
+    btn =gtk_button_new_with_label("Click me");
+    gtk_window_set_child(GTK_WINDOW(win), btn);
+    g_signal_connect(btn,"clicked",G_CALLBACK(onclick1), win);
     gtk_widget_show(win);
 }
 
